a08dfd3d65e788d7111afc27ec982567eab7981f
Shabab Noor <ashababnoor@gmail.com>
2023-10-04 00:14:17 +0600

    bug fix in hex color code

--- python/hex_color_code.py
+++ python/hex_color_code.py
@@ -12,10 +12,12 @@ for _ in range(n):
     lines.append(input().rstrip())
 
 property_finder = r'.*:.*;'
-hex_finder = r'#([0-9a-fA-f]{3}|[0-9a-fA-F]{6})'
+hex_finder = r'(#[0-9a-fA-f]{6}|#[0-9a-fA-F]{3})'
 
 for line in lines:
     if len(re.findall(property_finder, line)) > 0:
+        
         color_codes = re.findall(hex_finder, line)
+        
         for color_code in color_codes: 
             print(color_code)
