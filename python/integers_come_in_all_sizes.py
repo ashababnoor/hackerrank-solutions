7cd70ac1ea9851450868847a2af5c2109a23368c
Shabab Noor <ashababnoor@gmail.com>
2023-10-03 23:29:07 +0600

    added integers come in all sizes in python

---
+++ python/integers_come_in_all_sizes.py
@@ -0,0 +1,11 @@
+'''
+Integers Come In All Sizes
+Link: https://www.hackerrank.com/challenges/python-integers-come-in-all-sizes/problem
+'''
+
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+a = int(input())
+b = int(input())
+c = int(input())
+d = int(input())
+print(a**b + c**d)
