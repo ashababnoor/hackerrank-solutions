832084392d30e87cb233c8b5bc8f018cd66c2ffb
Shabab Noor <ashababnoor@gmail.com>
2023-10-07 23:41:07 +0600

    added triangle quest in python/math

---
+++ python/math/triange_quest.py
@@ -0,0 +1,9 @@
+'''
+Triangle Quest
+Link: https://www.hackerrank.com/challenges/python-quest-1/problem
+'''
+
+for i in range(1, int(input())):
+    # More than 2 lines will result in 0 score. Do not leave a blank line also
+    num = sum([i*10**j for j in range(i)])
+    print(num)
