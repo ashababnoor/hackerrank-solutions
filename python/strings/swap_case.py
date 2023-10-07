bbdd26fde90300d505aff9b67a8bf0b33c4c81e8
Shabab Noor <ashababnoor@gmail.com>
2023-10-07 23:32:14 +0600

    added swap case in python/strings

---
+++ python/strings/swap_case.py
@@ -0,0 +1,12 @@
+'''
+Swap Case
+Link: https://www.hackerrank.com/challenges/swap-case/problem
+'''
+
+def swap_case(s):
+    return s.swapcase()
+
+if __name__ == '__main__':
+    s = input()
+    result = swap_case(s)
+    print(result)
