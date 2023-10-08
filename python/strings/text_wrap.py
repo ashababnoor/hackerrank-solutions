d591a98e145b17e7df3cda30b291aa1fbccea943
Shabab Noor <ashababnoor@gmail.com>
2023-10-08 23:37:43 +0600

    added alternate method in text wrap in python/strings

--- python/strings/text_wrap.py
+++ python/strings/text_wrap.py
@@ -8,6 +8,11 @@ import textwrap
 def wrap(string, max_width):
     return "\n".join([string[i:i+max_width] for i in range(0, len(string), max_width)])
 
+# alternate method using built-in function
+def wrap_(string, max_width):
+    result = textwrap.fill(string, max_width)
+    return result
+
 if __name__ == '__main__':
     string, max_width = input(), int(input())
     result = wrap(string, max_width)
