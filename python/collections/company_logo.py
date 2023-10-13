51418ab46868d46ed9fa882a9d9601dde0a66736
Shabab Noor <ashababnoor@gmail.com>
2023-10-13 23:29:49 +0600

    added company logo in python/collections

---
+++ python/collections/company_logo.py
@@ -0,0 +1,18 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+from collections import Counter
+
+
+if __name__ == '__main__':
+    s = list(input())
+    counter = Counter(s)
+
+    top_three = sorted(counter.most_common(), key=lambda x: (-x[1], x[0]))[:3]
+        
+    for letter, count in sorted(top_three, key=lambda x: (-x[1], x[0])):
+        print(letter, count)
---
+++ sql/advanced-select/occupations.sql
@@ -0,0 +1,87 @@
+/*
+Enter your query here.
+*/
+with doctors as (
+    select
+        name
+        , row_number() over(partition by occupation order by name) as row_num
+    from occupations
+    where occupation = "Doctor"
+)
+
+, professors as (
+    select
+        name
+        , row_number() over(partition by occupation order by name) as row_num
+    from occupations
+    where occupation = "Professor"
+)
+
+, singers as (
+    select
+        name
+        , row_number() over(partition by occupation order by name) as row_num
+    from occupations
+    where occupation = "Singer"
+)
+
+, actors as (
+    select
+        name
+        , row_number() over(partition by occupation order by name) as row_num
+    from occupations
+    where occupation = "Actor"
+)
+
+, left_joined as (
+    select
+        row_num
+        , d.name as doctors
+        , p.name as professors
+        , s.name as singers
+        , a.name as actors
+    from doctors d
+    left join professors p 
+    using (row_num)
+    left join singers s
+    using (row_num)
+    left join actors a
+    using (row_num)
+)
+
+, right_joined as (
+    select
+        row_num
+        , d.name as doctors
+        , p.name as professors
+        , s.name as singers
+        , a.name as actors
+    from doctors d
+    right join professors p 
+    using (row_num)
+    right join singers s
+    using (row_num)
+    right join actors a
+    using (row_num)
+)
+
+, final_table as (
+    select
+        *
+        , row_number() over(partition by doctors, professors, singers, actors) final_row_num
+    from (
+        select * from left_joined
+        union all
+        select * from right_joined
+    ) as outer_joined
+)
+
+select
+    doctors
+    , professors
+    , singers
+    , actors
+from final_table
+where final_row_num = 1
+order by doctors
+
