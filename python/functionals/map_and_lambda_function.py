0ece47ff912723201dafd81ec072f1ff6f7be200
Shabab Noor <ashababnoor@gmail.com>
2023-10-15 01:57:15 +0600

    added map and lambda function in python/functionals

---
+++ python/functionals/map_and_lambda_function.py
@@ -0,0 +1,22 @@
+'''
+Map and Lambda Function
+Link: https://www.hackerrank.com/challenges/map-and-lambda-expression/problem
+'''
+
+cube = lambda x: x**3
+
+def fibonacci(n):
+    fibonacci_list = []
+    for _ in range(n):
+        fibonacci_list.append(fibonacci_nth_value(_))
+    return fibonacci_list
+
+def fibonacci_nth_value(n):
+    if n <= 1:
+        return n
+    else:
+        return fibonacci_nth_value(n-1) + fibonacci_nth_value(n-2)
+
+if __name__ == '__main__':
+    n = int(input())
+    print(list(map(cube, fibonacci(n))))
