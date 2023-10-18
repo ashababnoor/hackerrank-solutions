bed9f8114692e3f71bf0fb5aec0536c7666224a6
Shabab Noor <ashababnoor@gmail.com>
2023-10-18 08:50:14 +0600

    added python evaluation in python/built-ins

---
+++ python/built-ins/python_evaluation.py
@@ -0,0 +1,8 @@
+'''
+Python Evaluation
+Link: https://www.hackerrank.com/challenges/python-eval/problem
+'''
+
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+input_string = input()
+eval(input_string)
