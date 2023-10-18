5f53a722603126c439d112a2499e6fa342d0d427
Shabab Noor <ashababnoor@gmail.com>
2023-10-18 08:51:04 +0600

    updated init.sh from gitit.sh gist

--- init.sh
+++ init.sh
@@ -53,15 +53,15 @@ function get_last_commit_changes() {
     # Find the commit range of the last push
     local last_commit_hash=$(git log -n 1 --pretty=format:%H)
     local last_commit_short_hash=$(git rev-parse --short $last_commit_hash)
-    local last_commit_time=$(git log -n 1 --format="%cd")
+    local last_commit_time=$(git log -n 1 --format="%cd" --date=format:'%a %d %b %Y %H:%M:%S %z')
 
     # Show modified files in the last commit
     echo "Changes made in last commit: ${purple_bold}$last_commit_short_hash${reset} ($last_commit_time)"
     git diff --name-status $last_commit_hash^..$last_commit_hash | awk '
         BEGIN {
-            color_A = "\033[1;32m";  # Green
-            color_M = "\033[1;33m";  # Yellow
-            color_D = "\033[1;31m";  # Red
+            color_A = "\033[0;32m";  # Green
+            color_M = "\033[0;33m";  # Yellow
+            color_D = "\033[0;31m";  # Red
             color_fbk = "\033[0;36m" # Light Blue; Fallback color
             reset = "\033[0m";       # Reset color
         }
@@ -129,4 +129,4 @@ function git_add_commit_push() {
     get_last_commit_changes
 }
 
-alias gitit=git_add_commit_push 
+alias gitit=git_add_commit_push
