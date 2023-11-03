bdcc22257b863347debe67d1dc5205e4e908f16d
Shabab Noor <ashababnoor@gmail.com>
2023-11-03 21:36:32 +0600

    added handshake in maths/fundamentals

---
+++ mathematics/fundamentals/handshake.py
@@ -0,0 +1,36 @@
+'''
+Handshake
+Link: https://www.hackerrank.com/challenges/handshake/problem
+'''
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'handshake' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER n as parameter.
+#
+
+def handshake(n):
+    return int(((n-1)*n)/2)
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input().strip())
+
+    for t_itr in range(t):
+        n = int(input().strip())
+
+        result = handshake(n)
+
+        fptr.write(str(result) + '\n')
+
+    fptr.close()
