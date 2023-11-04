6492ab9395918b3b22f73b7914373f615526786d
Shabab Noor <ashababnoor@gmail.com>
2023-11-05 03:15:48 +0600

    updated differences and product in maths/fundamentals

--- mathematics/fundamentals/difference_and_product.py
+++ mathematics/fundamentals/difference_and_product.py
@@ -27,13 +27,23 @@ def solve(d, p):
     if d == 0 and p == 0:
         # only possible when (a, b) = (0, 0)
         return 1
+    if d != 0 and p == 1:
+        # if product is 1 then (a, b) = (0, 0)
+        return 0
+    if d == 0 and p == 1:
+        # if product is 1 then (a, b) = (0, 0)
+        return 2
+    if d != 2 and p == -1:
+        # if product is -1 then (a, b) = (1, -1) / (-1, 1)
+        return 0
+    if d == 2 and p == -1:
+        # if product is -1 then (a, b) = (1, -1) / (-1, 1)
+        return 2
     if d == 0 and p != int(p**0.5)**2:
         # if absolute difference is zero then product must be a square number 
         return 0
     if d == 0 and p == int(p**0.5)**2:
         return 2
-        
-    return 4
     
     # |a-b| = d
     #  axb  = p
@@ -45,6 +55,25 @@ def solve(d, p):
     a_minus_b = d
     a = (a_plus_b + a_minus_b)/2
     b = (a_plus_b - a_minus_b)/2
+    if isinstance(a, complex):
+        # a is an integer
+        return 0
+    if isinstance(b, complex):
+        # b is an integer
+        return 0
+    if a != int(a):
+        # a is an integer
+        return 0
+    if b != int(b):
+        # b is an integer
+        return 0
+    if p < 0 and abs(a) + abs(b) != d:
+        # if product is negative then one number is negative and the other is positive
+        # i.e. the difference will be equal to the sum of the absolute values
+        return 0
+    
+    return 4
+
 
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
